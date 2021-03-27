f8a1850a3372613379c1fd5691df7eec66fff011
tanvira <tanvira1993@gmail.com>
2021-03-28 04:46:07 +0600

    secured private keys

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
+require('dotenv').config()
 module.exports={
-    MONGODB:"mongodb+srv://tanvir:44HsqhENLV3ln8Oz@cluster0.xljmh.mongodb.net/blog?retryWrites=true&w=majority",
-    SECRET_KEY:"JWT_SECRET"
+    MONGODB: process.env.MONGODB,
+    SECRET_KEY:process.env.SECRET_KEY
 }
